58b8214a2f10d0a34ce38e3e476b69f7480806a7
Adeeb Siddiqui <adeeb.bd@gmail.com>
2021-02-24 20:08:18 -0600

    Removed unimplemented methods from Calc interface

--- src/com/ad/Calc.java
+++ src/com/ad/Calc.java
@@ -3,7 +3,5 @@ package com.ad;
 public interface Calc {
 	
 	double add(double param1, double param2);
-	double subtract(double param1, double param2);
-	double multiply(double param1, double param2);
 	double divide(double param1, double param2);
 }
